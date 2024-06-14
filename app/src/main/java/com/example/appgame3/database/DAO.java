07afc425e35e69a9d741267425f9491af63e3114
SolKhanh <21130397@st.hcmuaf.edu.vn>
2024-06-14 20:13:37 +0700

    Chỉ giữ lại insert

--- app/src/main/java/com/example/appgame3/database/DAO.java
+++ app/src/main/java/com/example/appgame3/database/DAO.java
@@ -1,14 +1,10 @@
 package com.example.appgame3.database;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import com.example.appgame3.model.Level;
 
-import java.util.List;
-
 public interface DAO<T> {
-    void insert(T object);
-    Level findById(int id);
-    List<T> findAll();
-    int update(T object);
-    int delete(T object);
+    void insert(SQLiteDatabase db, T t);
 }
 
